8a337f77631919ba3531cb560ad2249da26865fe
Evelyn Torres <evelyn@MacBook-Air.hitronhub.home>
2022-07-10 07:32:07 -0700

    updating js file with functions

--- server.js
+++ server.js
@@ -1,106 +1,129 @@
 // Dependencies
-const mysql = require('mysql2');
-const inquirer = require('inquirer');
-
+const express = require('express');
+const mysql = require("mysql2");
+const inquirer = require("inquirer");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const cTable = require('console.table');
+const cTable = require("console.table");
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // Connection to MySQL
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3301,
-  user:"root",
-  password:"Password11!",
-  database:"employee_db"
-}
- console.log(`Connected to MySQL`)
+const connection = mysql.createConnection(
+  {
+    host: "localhost",
+    port: 3301,
+    user: "root",
+    password: "Password11!",
+    database: "employee_db",
+  },
+  console.log(`Connected to MySQL`)
 );
 
 // Connect to mySql server and database
-connection.connect((err) => {
-  if(err)throw err;
-// add start function here
+connection.connect(() => {
   start();
 });
 
 function start() {
-   inquirer
-   .prompt({
+  inquirer
+    .prompt({
       name: "selection",
       type: "list",
       message: "What would you like to do?",
-      choices: 
-        [
-            "View all departments",
-            "View all roles",
-            "View all employees", 
-            "Add a department",
-            "Add a role", 
-            "Add an employee",
-            "Update a role",
-        ]
-      })
-      .then(function(answer) {
-          console.log(answer);
-        
-        if (answer.selection === "View all departments") {
-          viewDepts();
-        }
-        else if(answer.selection === "View all roles") {
-          viewRole();
-    
-        } 
-        else if(answer.selection === "View all employees") {
-          viewAll();
-    
-        }
-        else if(answer.selection === "Add a department") {
-          addDepart();
-    
-        }
-        else if(answer.selection === "Add a role") {
-          addRole();
-    
-        }
-        else if(answer.selection === "Add an employee") {
-          addEmployee();
-    
-        }
-        else if(answer.selection === "Update a role") {
-          updateRole();
-    
-        }else{
-          connection.end();
-        }
-      });
-    }
+      choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update a role",
+      ],
+    })
+    .then(function (answer) {
+      console.log(answer);
+
+      if (answer.selection === "View all departments") {
+        viewDepts();
+      } else if (answer.selection === "View all roles") {
+        viewRole();
+      } else if (answer.selection === "View all employees") {
+        viewAllEmployees();
+      } else if (answer.selection === "Add a department") {
+        addDepartment();
+      } else if (answer.selection === "Add a role") {
+        addRole();
+      } else if (answer.selection === "Add an employee") {
+        addEmployee();
+      } else if (answer.selection === "Update a role") {
+        updateRole();
+      } else {
+        connection.end();
+      }
+    });
+}
 
 // View departments
-function viewDepts(){
+function viewDepts() {
   inquirer
     .prompt([
-          {
-            type:"list",
-            name:"viewDepts",
-            message:"Select one to view:",
-            choices: ["Management", "File office", "HR", "Sales"]
-        
-          }
-         ])
-         .then(function(res){
-             switch(res.view){
-              case "All employees":
-                  viewAllEmployees();
-                  break;
-              case "By department":
-                  viewByDepartment();
-                  break;
-              case "By role":
-                  viewByRole();I
-              default:
-                    console.log("default");
-             }
-            });
-          }
+      {
+        type: "list",
+        name: "viewDepts",
+        message: "Select one to view:",
+        choices: ["Management", "File office", "HR", "Sales"],
+      },
+    ])
+  }
+// 
+
+// // function to view all departments
+
+// const viewDepts = () => {
+//   db.query("SELECT * FROM department", (err, res) => {
+//     if (err)
+//     throw err;
+//     console.table(res)
+//     start();
+//   })
+// }
+
+
+
+// View roles
+function viewRole() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "viewRole",
+        message: "Select one to view:",
+        choices: ["Store manager", "Assistant manager", "Meat manager", "Lead", "Accountant","Meat clerk", "Grocery clerk" ],
+      },
+    ])
+  
+}
+
+
+// const viewRole = () => {
+//   db.query("SELECT * FROM role", (err, res) => {
+//     if (err)
+//     throw err;
+//     console.table(res)
+//     start();
+//   })
+// }
+
+// function to view all employees
+
+const viewAllEmployees = () => {
+  db.query("SELECT first_name, last_name FROM employee", (err, res) => {
+    if (err)
+    throw err;
+    console.table(res)
+    start();
+  })
+}
