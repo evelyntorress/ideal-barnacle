cca0279b60d85cf935a825a853f7c6a327a007d1
Evelyn Torres <evelyn@MacBook-Air.hitronhub.home>
2022-07-10 11:22:12 -0700

    updating functions in js file

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const inquirer = require("inquirer");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const cTable = require("console.table");
+const table = require("console.table");
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -14,7 +14,7 @@ app.use(express.json());
 const connection = mysql.createConnection(
   {
     host: "localhost",
-    port: 3301,
+    port: 3306,
     user: "root",
     password: "Password11!",
     database: "employee_db",
@@ -23,10 +23,12 @@ const connection = mysql.createConnection(
 );
 
 // Connect to mySql server and database
-connection.connect(() => {
+connection.connect(function (err) {
+  if (err) throw err;
   start();
 });
 
+//  Starts the prompt with questions
 function start() {
   inquirer
     .prompt({
@@ -67,20 +69,6 @@ function start() {
 }
 
 // View departments
-// function viewDepts() {
-//   inquirer
-//     .prompt([
-//       {
-//         type: "list",
-//         name: "viewDepts",
-//         message: "Select one to view:",
-//         choices: ["Management", "File office", "HR", "Sales"],
-//       },
-//     ])
-//   }
-// 
-
-// function to view all departments
 
 function viewDepts() {
   connection.query("SELECT * FROM department", (err, res) => {
@@ -90,40 +78,109 @@ function viewDepts() {
     start();
   })
 }
-
-
-
 // View roles
-function viewRole() {
-  inquirer
-    .prompt([
-      {
-        type: "list",
-        name: "viewRole",
-        message: "Select one to view:",
-        choices: ["Store manager", "Assistant manager", "Meat manager", "Lead", "Accountant","Meat clerk", "Grocery clerk" ],
-      },
-    ])
-  
-}
 
-
-// const viewRole = () => {
-//   db.query("SELECT * FROM role", (err, res) => {
-//     if (err)
-//     throw err;
-//     console.table(res)
-//     start();
-//   })
-// }
-
-// function to view all employees
-
-const viewAllEmployees = () => {
-  connection.query("SELECT first_name, last_name FROM employee", (err, res) => {
+function viewRole () {
+  connection.query("SELECT * FROM role", (err, res) => {
     if (err)
     throw err;
     console.table(res)
     start();
   })
 }
+
+// View all employees
+
+function viewAllEmployees () {
+  connection.query("SELECT first_name, last_name FROM employee LEFT JOIN department_name FROM department", (err, res) => {
+    if (err)
+    throw err;
+    console.table(res)
+    start();
+  })
+}
+
+// Add department
+
+function addDepartment() {
+  inquirer
+  .prompt([
+      {
+          type: "input",
+          name: "department",
+          message: "Insert a new department",
+      }
+    ])
+  .then(function(answer) {
+      connection.query("INSERT INTO department VALUE (DEFAULT, ?)",
+    [answer.department],
+      function(err){
+          if (err)
+          throw err;
+      console.log (`Department added.`);
+      start();
+      })
+  })
+}
+
+// Add role
+
+function addRole() {
+
+  inquirer
+  .prompt([
+    {
+    type: "input",
+    name: "role",
+    message: "Role title",
+    },
+    {
+    type: "number",
+    name: "salary",
+    message: "Enter salary",
+    },
+    {
+    type: "list",
+    name: "department_id",
+    message: "Enter the Department id ",
+    }
+  ])
+  .then(function(answer) {
+    connection.query("INSERT INTO role SET ?",
+  [answer.role],
+    function(err){
+        if (err)
+        throw err;
+    console.log (`Role added.`);
+    start();
+    })
+})
+}
+
+
+
+
+
+
+
+//   connection.query("SELECT * FROM department", (err, res) => {
+//       if (err)
+//       throw err;
+
+//       ]) .then(data => {
+//           // returns name of the department depID.id (comes from integer created from table creation)
+//           const depID = res.find(department => department.dep_name === data.depID)
+//           db.query("INSERT INTO role_table SET ?", {
+//               title: data.role, salary: data.salary, department_id: depID.id
+//           }, 
+//           err => {
+//               if (err) 
+//               throw err;
+                  
+//           console.log(`Role successfully added. Select "view all roles" to see addition`)
+//           promptUser(); 
+//       }
+//           ) 
+//       })
+//   })
+// }
